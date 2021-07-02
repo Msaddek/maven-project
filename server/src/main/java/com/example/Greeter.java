1a02ce9b4b31550760b64d1306a3b7ebbf1693da
Msaddek <80820391+Msaddek@users.noreply.github.com>
2021-07-02 15:13:31 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -6,7 +6,10 @@ public class Greeter {
   public Greeter() {
 
   }
-// Methode de salutation
+/**
+* @param someone name of person
+* @return greeting string
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
